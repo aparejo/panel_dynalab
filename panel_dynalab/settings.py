3e2b994fd8f2de454fed7cc1994f5fb05a3fa812
Antonio Parejo <antonioparejo@gmail.com>
2024-12-11 08:44:23 -0400

    Header-titulo y settings

--- panel_dynalab/settings.py
+++ panel_dynalab/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -75,6 +76,10 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'panel_dynalab.wsgi.application'
+
+CORS_ALLOWED_ORIGINS = [
+    'https://web.dynalabglobal.com',
+]
 
 
 # Database
